9c935d1eb42ebbdbb181b53fc5d8fedbb45ead0e
dherrada <dylan.herrada@adafruit.com>
2021-11-05 17:14:37 -0400

    Linted

--- adafruit_tsl2591.py
+++ adafruit_tsl2591.py
@@ -28,7 +28,7 @@ Implementation Notes
 """
 from micropython import const
 
-import adafruit_bus_device.i2c_device as i2c_device
+from adafruit_bus_device import i2c_device
 
 
 __version__ = "0.0.0-auto.0"
