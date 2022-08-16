fc6783c80a1fc383ae80286263dd15f3ce6b33e1
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_tsl2591.py
+++ adafruit_tsl2591.py
@@ -31,7 +31,7 @@ from micropython import const
 from adafruit_bus_device import i2c_device
 
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_TSL2591.git"
 
 
