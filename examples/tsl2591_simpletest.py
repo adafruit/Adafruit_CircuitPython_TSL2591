2ebbc504d48c749761763abdb5302fde9f57f835
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:31 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/tsl2591_simpletest.py
+++ examples/tsl2591_simpletest.py
@@ -9,6 +9,7 @@ import adafruit_tsl2591
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
 # Initialize the sensor.
 sensor = adafruit_tsl2591.TSL2591(i2c)
